98fc2211aa7f30726dd8654927a39eac42e15e88
Shehanka <chamodp@wso2.com>
2022-03-08 13:34:47 +0530

    Triggers data checking

--- src/services/ballerina-trigger.service.js
+++ src/services/ballerina-trigger.service.js
@@ -45,7 +45,7 @@ class BallerinaTriggerService {
     try {
       console.log("getting triggers by id ......");
       const res = await axios.get(PATH);
-
+      console.log("Triggers  data : ", res.data);
       return res.data;
     } catch (e) {
       console.error(
