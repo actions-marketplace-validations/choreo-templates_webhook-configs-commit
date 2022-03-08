03e40800580c68989239b9d710acd2836666095c
Shehanka <chamodp@wso2.com>
2022-03-08 10:22:41 +0530

    Buffer module added

--- index.js
+++ index.js
@@ -1,16 +1,64 @@
-import * as core from '@actions/core';
+import * as core from "@actions/core";
+import { BallerinaTriggerService } from "./src/services/ballerina-trigger.service";
+import { GitHubService } from "./src/services/github.service";
+import { triggerTemplateHbs } from "./src/config/constants";
 
 try {
-    const token = core.getInput('token');
-    const environment = core.getInput('environment');
-    const org = core.getInput('org');
-    const repo = core.getInput('repo');
-    const branch = core.getInput('branch');
-    const isHttpBased = core.getInput('isHttpBased');
+  const token = core.getInput("token");
+  const environment = core.getInput("environment");
+  const org = core.getInput("org");
+  const repo = core.getInput("repo");
+  const branch = core.getInput("branch");
+  const isHttpBased = core.getInput("isHttpBased");
+  const ballerinaTriggerID = parseInt(core.getInput("ballerinaTriggerID"));
 
+  const ballerinaTriggerService = new BallerinaTriggerService(
+    environment,
+    ballerinaTriggerID,
+    isHttpBased === "true"
+  );
+
+  const githubService = new GitHubService(token, org, repo);
+
+  ballerinaTriggerService
+    .getFilteredTriggers()
+    .then((filteredTriggers) => {
+      const base64Payload = Buffer.from(
+        JSON.stringify(filteredTriggers)
+      ).toString("base64");
+
+      githubService
+        .createBranch(branch)
+        .then(() => {
+          githubService
+            .createMultipleFiles(branch, "Choreo Webhook Configs Added", [
+              { data: base64Payload, path: "data.json" },
+              { data: triggerTemplateHbs, path: "triggertemplate.hbs" },
+            ])
+            .then(() => {
+              console.log(`Config files committed successfully`);
+              core.setOutput("status", "success");
+            })
+            .catch((e) => {
+              core.setOutput("status", "failure");
+              console.error(`Error: ${e}`);
+              core.setOutput("error", e.message);
+            });
+        })
+        .catch((e) => {
+          core.setOutput("status", "failure");
+          console.error(`Error: ${e}`);
+          core.setOutput("error", e.message);
+        });
+    })
+    .catch((e) => {
+      core.setOutput("status", "failure");
+      console.error(`Error: ${e}`);
+      core.setOutput("error", e.message);
+    });
 } catch (e) {
-    core.setOutput("choreo-webhook-config-commit-status", "failed");
-    core.setFailed(e.message);
-    console.log("choreo-webhook-config-commit-status", "failed");
-    console.log(e.message);
+  core.setOutput("choreo-webhook-config-commit-status", "failed");
+  core.setFailed(e.message);
+  console.log("choreo-webhook-config-commit-status", "failed");
+  console.log(e.message);
 }
